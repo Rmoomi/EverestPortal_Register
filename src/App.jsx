9d7b9c719b9a57801761c4a2989f149c515c6b36
Rmoomi <aarondimzon23@gmail.com>
2025-08-22 11:08:55 +0800

    Changed BrowserRouter to HashRouter for GitHub Pages fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import RegisterAcc from "./components/Register.jsx";
 import Homepage from "./components/Home_page.jsx";
 import Login from "./components/Login.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} /> {/* Root/default route */}
         <Route path="/register" element={<RegisterAcc />} />
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
